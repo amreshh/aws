0663f2b66531e8f1422c76a418e40ca6ce606ae9
Amresh Hanoeman <20923769+amreshh@users.noreply.github.com>
2024-03-03 18:34:04 +0100

    nat gw per az
    Signed-off-by: Amresh Hanoeman <20923769+amreshh@users.noreply.github.com>

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -4,6 +4,8 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   vpc_config {
     subnet_ids = var.subnet_ids
+    endpoint_private_access = false 
+    public_access_cidrs = ["0.0.0.0/0"]
   }
 
   depends_on = [
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -74,28 +74,30 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
+  for_each = toset(aws_nat_gateway.private_nat_gateway[*].id)
+  vpc_id   = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_nat_gateway[0].id
+    gateway_id = each.key
   }
+
   tags = {
-    Name = "private route table"
+    Name = "private route table ${each.key}"
   }
 }
 
-# resource "aws_route_table_association" "private_subnet_association" {
-#   count          = length(var.private_subnet_cidrs)
-#   subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
-#   route_table_id = aws_default_route_table.default_route_table.id
-# }
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
 
-# resource "aws_route_table_association" "public_subnet_association" {
-#   count          = length(var.public_subnet_cidrs)
-#   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-#   route_table_id = aws_route_table.public_route_table.id
-# }
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_route_table[aws_nat_gateway.private_nat_gateway[count.index].id].id
+}
 
 # resource "aws_default_security_group" "default" {
 #   vpc_id = aws_vpc.main.id
